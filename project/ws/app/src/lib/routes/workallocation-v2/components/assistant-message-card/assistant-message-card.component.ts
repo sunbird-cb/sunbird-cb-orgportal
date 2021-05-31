d52b0c39a2c8a12de781df616b2c04d92a28e783
christopher.fernandes <christyabcd.05@gmail.com>
2021-05-31 20:40:46 +0530

    Task #54483: changed the component logic errors/warning

--- project/ws/app/src/lib/routes/workallocation-v2/components/assistant-message-card/assistant-message-card.component.ts
+++ project/ws/app/src/lib/routes/workallocation-v2/components/assistant-message-card/assistant-message-card.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core'
+import { IWarnError } from '../../models/warn-error.model'
+// tslint:disable
+import _ from 'lodash'
+// tslint:enable
 
 @Component({
   selector: 'ws-app-assistant-message-card',
@@ -7,14 +11,84 @@ import { Component, Input, OnInit } from '@angular/core'
 })
 
 export class AssistantMessageCardComponent implements OnInit {
-  @Input() type: 'activity' | 'role' | 'competency' | 'officer' = 'activity'
-  @Input() count = 0
-  @Input() message = ''
+  @Input() dataStructure: any
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  get allWarning() {
+    let warnings: IWarnError[] = []
+    let calculatedWarn: IWarnError[] = []
+    let calculatedWarn2: IWarnError[] = []
+    if (this.dataStructure.activityGroups) {
+      calculatedWarn = this.calculateWarn(this.dataStructure.activityGroups)
+    }
+    if (this.dataStructure.officerFormData) {
+      calculatedWarn2 = this.calculateOfficerWarning(this.dataStructure.officerFormData)
+    }
+    warnings = _.union(calculatedWarn, calculatedWarn2)
+    return warnings
+  }
+  calculateWarn(data: any[]): IWarnError[] {
+    const result: IWarnError[] = []
+    const grpDescEmpty = Math.max(_.filter(data, () => ['groupDescription', 'Untited role']).length - 1, 0)
+    if (grpDescEmpty) {
+      result.push({ _type: 'warning', type: 'role', counts: grpDescEmpty, label: 'Role description missing' })
+    }
+    // const unmapedActivities = _.size(_.get(_.first(data), 'activities'))
+    // if (unmapedActivities) {
+    //   result.push({ _type: 'warning', type: 'activity', counts: unmapedActivities, label: 'Unmapped activities' })
+    // }
+
+    return result
+  }
+
+  calculateOfficerWarning(data: any): IWarnError[] {
+    const result: IWarnError[] = []
+    // console.log('data------', data)
+    if (data && data.positionDescription === '') {
+      result.push({ _type: 'warning', type: 'officer', counts: 0, label: 'Position description missing' })
+    }
+    return result
+  }
+
+  get allErrors() {
+    let errors: IWarnError[] = []
+    let calculatedErr: IWarnError[] = []
+    let calculatedErr2: IWarnError[] = []
+    if (this.dataStructure.officerFormData) {
+      calculatedErr = (this.calculateOfficerErrors(this.dataStructure.officerFormData))
+    }
+    if (this.dataStructure.activityGroups) {
+      calculatedErr2 = this.calculateActivityError(this.dataStructure.activityGroups)
+    }
+
+    errors = _.union(calculatedErr, calculatedErr2)
+    return errors
+  }
+
+  calculateOfficerErrors(data: any): IWarnError[] {
+    const result: IWarnError[] = []
+    // console.log('data------', data)
+    if (data && data.officerName === '' && (data.position !== '' || data.positionDescription !== '')) {
+      result.push({ _type: 'error', type: 'officer', counts: 0, label: 'Officer name is empty' })
+    }
+    if (data && data.position === '' && (data.officerName !== '' || data.positionDescription !== '')) {
+      result.push({ _type: 'error', type: 'officer', counts: 0, label: 'Postion missing' })
+    }
+    return result
+  }
+
+  calculateActivityError(data: any): IWarnError[] {
+    const result: IWarnError[] = []
+    const unmapedActivities = _.size(_.get(_.first(data), 'activities'))
+    if (unmapedActivities) {
+      result.push({ _type: 'warning', type: 'activity', counts: unmapedActivities, label: 'Unmapped activities' })
+    }
+    return result
   }
 
 }
--- project/ws/app/src/lib/routes/workallocation-v2/routes/create-workallocation/create-workallocation.component.ts
+++ project/ws/app/src/lib/routes/workallocation-v2/routes/create-workallocation/create-workallocation.component.ts
@@ -8,7 +8,6 @@ import { NSWatActivity } from '../../models/activity-wot.model'
 import { NSWatCompetency } from '../../models/competency-wat.model'
 // tslint:enable
 // import { NSWatActivity } from '../../models/activity-wot.model'
-import { IWarnError } from '../../models/warn-error.model'
 import { AllocationService } from '../../services/allocation.service'
 import { WatStoreService } from '../../services/wat.store.service'
 
@@ -247,77 +246,6 @@ export class CreateWorkallocationComponent implements OnInit, AfterViewInit, OnD
     this.snackBar.open(primaryMsg, 'X', {
       duration,
     })
-  }
-  get allWarning() {
-    let warnings: IWarnError[] = []
-    let calculatedWarn: IWarnError[] = []
-    let calculatedWarn2: IWarnError[] = []
-    if (this.dataStructure.activityGroups) {
-      calculatedWarn = this.calculateWarn(this.dataStructure.activityGroups)
-    }
-    if (this.dataStructure.officerFormData) {
-      calculatedWarn2 = this.calculateOfficerWarning(this.dataStructure.officerFormData)
-    }
-    warnings = _.union(calculatedWarn, calculatedWarn2)
-    return warnings
-  }
-  calculateWarn(data: any[]): IWarnError[] {
-    const result: IWarnError[] = []
-    const grpDescEmpty = Math.max(_.filter(data, () => ['groupDescription', 'Untited role']).length - 1, 0)
-    if (grpDescEmpty) {
-      result.push({ _type: 'warning', type: 'role', counts: grpDescEmpty, label: 'Role description missing' })
-    }
-    // const unmapedActivities = _.size(_.get(_.first(data), 'activities'))
-    // if (unmapedActivities) {
-    //   result.push({ _type: 'warning', type: 'activity', counts: unmapedActivities, label: 'Unmapped activities' })
-    // }
-
-    return result
-  }
-
-  calculateOfficerWarning(data: any): IWarnError[] {
-    const result: IWarnError[] = []
-    // console.log('data------', data)
-    if (data && data.positionDescription === '') {
-      result.push({ _type: 'warning', type: 'officer', counts: 0, label: 'Position description missing' })
-    }
-    return result
-  }
-
-  get allErrors() {
-    let errors: IWarnError[] = []
-    let calculatedErr: IWarnError[] = []
-    let calculatedErr2: IWarnError[] = []
-    if (this.dataStructure.officerFormData) {
-      calculatedErr = (this.calculateOfficerErrors(this.dataStructure.officerFormData))
-    }
-    if (this.dataStructure.activityGroups) {
-      calculatedErr2 = this.calculateActivityError(this.dataStructure.activityGroups)
-    }
-
-    errors = _.union(calculatedErr, calculatedErr2)
-    return errors
-  }
-
-  calculateOfficerErrors(data: any): IWarnError[] {
-    const result: IWarnError[] = []
-    // console.log('data------', data)
-    if (data && data.officerName === '' && (data.position !== '' || data.positionDescription !== '')) {
-      result.push({ _type: 'error', type: 'officer', counts: 0, label: 'Officer name is empty' })
-    }
-    if (data && data.position === '' && (data.officerName !== '' || data.positionDescription !== '')) {
-      result.push({ _type: 'error', type: 'officer', counts: 0, label: 'Postion missing' })
-    }
-    return result
-  }
-
-  calculateActivityError(data: any): IWarnError[] {
-    const result: IWarnError[] = []
-    const unmapedActivities = _.size(_.get(_.first(data), 'activities'))
-    if (unmapedActivities) {
-      result.push({ _type: 'warning', type: 'activity', counts: unmapedActivities, label: 'Unmapped activities' })
-    }
-    return result
   }
   ngOnDestroy() {
     this.activitySubscription.unsubscribe()
