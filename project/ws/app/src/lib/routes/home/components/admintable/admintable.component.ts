e93c47cc9df7fa6dc9d2f59e058ef2ac54145824
NandiniAV <43979758+NandiniAV@users.noreply.github.com>
2021-11-24 14:48:52 +0530

    Dev mdoinfo (#74) (#75)
    * Fixed search message issues & user listing issue
    
    * Fixed issue in update staff

--- project/ws/app/src/lib/routes/home/components/admintable/admintable.component.ts
+++ project/ws/app/src/lib/routes/home/components/admintable/admintable.component.ts
@@ -151,6 +151,8 @@ export class AdmintableComponent implements OnInit, OnChanges {
               this.dataSource.paginator = this.paginator
               this.getAllUsers(this.deptID)
             }
+          } else {
+            this.getAllUsers(this.deptID)
           }
         },
         (_err: any) => {
