f450e9d2db8270b19f3380a026fdb8998678a764
Vishali Sakar <vishali.sakar@tarento.com>
2021-12-29 15:44:05 +0530

    fix: sidemenu telemetry event added

--- project/ws/app/src/lib/routes/workallocation/routes/update-workallocation/update-workallocation.component.ts
+++ project/ws/app/src/lib/routes/workallocation/routes/update-workallocation/update-workallocation.component.ts
@@ -6,7 +6,8 @@ import { MatSnackBar } from '@angular/material'
 import { AllocationService } from '../../services/allocation.service'
 import { ConfigurationsService, EventService } from '@sunbird-cb/utils'
 import { TelemetryEvents } from '../../../../head/_services/telemetry.event.model'
-import _ from 'lodash'
+import * as _ from "lodash"
+
 
 @Component({
   selector: 'ws-app-update-workallocation',
