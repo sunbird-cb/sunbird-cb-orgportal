eb478be9afd6b306a702ce71dab58ece88b3174e
christopher.fernandes <christyabcd.05@gmail.com>
2022-06-29 11:39:18 +0530

    Task #58726: consultancy form integration

--- project/ws/app/src/lib/routes/state-profile/routes/consultancy/consultancy.component.ts
+++ project/ws/app/src/lib/routes/state-profile/routes/consultancy/consultancy.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
-import { debounceTime, switchMap, takeUntil } from 'rxjs/operators'
 import { OrgProfileService } from '../../services/org-profile.service'
 import { Subject } from 'rxjs'
+import { ConfigurationsService } from '@sunbird-cb/utils'
+/* tslint:disable*/
+import _ from 'lodash'
+import { Router } from '@angular/router'
+import { DialogConfirmComponent } from '../../../../../../../../../src/app/component/dialog-confirm/dialog-confirm.component'
 
 @Component({
     selector: 'ws-app-consultancy',
@@ -14,35 +18,34 @@ import { Subject } from 'rxjs'
 })
 export class ConsultancyComponent implements OnInit {
     consultancyForm!: FormGroup
+    addedconsultancies: any[] = []
+    @ViewChild('deleteTitleRef', { static: true })
+    deleteTitleRef: ElementRef | null = null
+    @ViewChild('deleteBodyRef', { static: true })
+    deleteBodyRef: ElementRef | null = null
+    editProgramValue: any
+    textBoxActive = false
     private unsubscribe = new Subject<void>()
 
     constructor(
         private orgSvc: OrgProfileService,
+        private configSvc: ConfigurationsService,
+
     ) {
         this.consultancyForm = new FormGroup({
             projectName: new FormControl('', [Validators.required]),
             programeStatus: new FormControl('', [Validators.required]),
             industrySponsored: new FormControl('', [Validators.required]),
             govtSponsored: new FormControl('', [Validators.required]),
+            otherSponsored: new FormControl('', [Validators.required]),
             projectDetail: new FormControl('', [Validators.required]),
-            // industrySponsoreName: new FormControl('', [Validators.required]),
-            // industrySponsoreDetail: new FormControl('', [Validators.required]),
-            // govSponsoreName: new FormControl('', [Validators.required]),
-            // govSponsoreDetail: new FormControl('', [Validators.required]),
-            // ongoingProjectName: new FormControl('', [Validators.required]),
-            // ongoingProjectDetail: new FormControl('', [Validators.required])
         })
 
-        this.consultancyForm.valueChanges
-            .pipe(
-                debounceTime(500),
-                switchMap(async formValue => {
-                    if (formValue) {
-                        this.orgSvc.updateLocalFormValue('consultancy', formValue)
-                    }
-                }),
-                takeUntil(this.unsubscribe)
-            ).subscribe()
+        // pre poluate form fields when data is available (edit mode)
+        if (this.configSvc.unMappedUser && this.configSvc.unMappedUser.orgProfile) {
+            const consultancyData = _.get(this.configSvc.unMappedUser.orgProfile, 'profileDetails.consultancy')
+            this.addedconsultancies = _.get(consultancyData, 'consultancies') || []
+        }
     }
 
     ngOnInit() {
