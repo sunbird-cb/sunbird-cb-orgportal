221c116740984b08fcbc4fbaaea8e249cea74355
christopher.fernandes <christyabcd.05@gmail.com>
2022-12-01 10:32:07 +0530

    Task #60298: Mandatory course routing

--- project/ws/app/src/lib/routes/mandatory-courses/mandatory-courses-routing.module.ts
+++ project/ws/app/src/lib/routes/mandatory-courses/mandatory-courses-routing.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
-import { MandatoryCoursesHomeComponent } from './routes/mandatory-courses-home/mandatory-courses-home/mandatory-courses-home.component'
-
+import { AddCoursesComponent } from './routes/add-courses/add-courses.component'
+import { MandatoryCourseHomeComponent } from './routes/mandatory-course-home/mandatory-course-home.component'
+import { MandatoryCourseComponent } from './routes/mandatory-courses-home/mandatory-courses-home/mandatory-course.component'
 
 const routes: Routes = [
-
   {
-    path: ':id',
-    component: MandatoryCoursesHomeComponent,
+    path: '',
+    component: MandatoryCourseHomeComponent,
+    children: [
+      {
+        path: ':id',
+        component: MandatoryCourseComponent,
+      },
+      {
+        path: ':id/courses',
+        component: AddCoursesComponent,
+      },
+    ],
   },
-
 ]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class MandatoryCoursesRoutingModule { }
--- project/ws/app/src/lib/routes/mandatory-courses/mandatory-courses.module.ts
+++ project/ws/app/src/lib/routes/mandatory-courses/mandatory-courses.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { MandatoryCoursesRoutingModule } from './mandatory-courses-routing.module';
-import { MandatoryCoursesHomeComponent } from './routes/mandatory-courses-home/mandatory-courses-home/mandatory-courses-home.component';
-
+import { MandatoryCoursesRoutingModule } from './mandatory-courses-routing.module'
+import { MandatoryCourseComponent } from './routes/mandatory-courses-home/mandatory-courses-home/mandatory-course.component'
+import { NoDataComponent } from './components/no-data/no-data.component'
+import { MandatoryCourseHomeComponent } from './routes/mandatory-course-home/mandatory-course-home.component'
+import { AddCoursesComponent } from './routes/add-courses/add-courses.component'
 
 @NgModule({
-  declarations: [MandatoryCoursesHomeComponent],
+  declarations: [MandatoryCourseComponent, NoDataComponent, MandatoryCourseHomeComponent, AddCoursesComponent],
   imports: [
     CommonModule,
-    MandatoryCoursesRoutingModule
-  ]
+    MandatoryCoursesRoutingModule,
+  ],
 })
 export class MandatoryCoursesModule { }
