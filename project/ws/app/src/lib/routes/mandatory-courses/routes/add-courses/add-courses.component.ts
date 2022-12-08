19ba076a80ff7b888b1cd3029063421fc9e7320d
nitin raj <nitin.raj@tarento.com>
2022-12-08 11:52:34 +0530

    course cards added for choose course page

--- project/ws/app/src/lib/routes/mandatory-courses/routes/add-courses/add-courses.component.ts
+++ project/ws/app/src/lib/routes/mandatory-courses/routes/add-courses/add-courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { MandatoryCourseService } from '../../services/mandatory-course.service'
 
 @Component({
   selector: 'ws-app-add-courses',
@@ -11,9 +12,38 @@ export class AddCoursesComponent implements OnInit {
     { title: 'Folder name', url: '/app/mandatory-courses/132' },
     { title: 'Choose courses', url: 'none' },
   ]
-  constructor() { }
+  searchResults: any = []
+  constructor(private mandatoryCourseSvc: MandatoryCourseService) { }
 
   ngOnInit() {
+    this.getSearchedData()
+  }
+
+  getSearchedData() {
+    const queryparam = {
+      request: {
+        filters: {
+          contentType: [],
+          primaryCategory: [],
+          mimeType: [],
+          source: [],
+          mediaType: [],
+          status: ['Live'],
+          'competencies_v3.name': [],
+          topics: [],
+        },
+        query: '',
+        sort_by: { lastUpdatedOn: 'desc' },
+        fields: [],
+        facets: ['primaryCategory', 'mimeType', 'source', 'competencies_v3.name', 'topics'],
+        limit: 100,
+        offset: 0,
+        fuzzy: true,
+      },
+    }
+    this.mandatoryCourseSvc.fetchSearchData(queryparam).subscribe((response: any) => {
+      this.searchResults = response.result.content
+    })
   }
 
 }
