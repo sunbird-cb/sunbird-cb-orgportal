ee7b64ccedd20985c8bf4f4498886d64cbda14a1
NandiniAV <nandini.achutha@tarento.com>
2023-06-14 16:51:13 +0530

    Updated left sidenav code for blended pages

--- project/ws/app/src/lib/routes/blended-program-approvals/components/blended-home/blended-home.component.ts
+++ project/ws/app/src/lib/routes/blended-program-approvals/components/blended-home/blended-home.component.ts
@@ -23,12 +23,14 @@ export class BlendedHomeComponent implements OnInit {
   role: any
 
   constructor(private router: Router, private activeRoute: ActivatedRoute) {
+    if (_.get(this.activeRoute, 'snapshot.data.configService.userRoles')) {
+      this.myRoles = _.get(this.activeRoute, 'snapshot.data.configService.userRoles')
+    }
     this.router.events.subscribe((event: Event) => {
       if (event instanceof NavigationEnd) {
         this.bindUrl(event.urlAfterRedirects.replace('/app/blended-approvals/', ''))
         const fullProfile = _.get(this.activeRoute.snapshot, 'data.configService')
         const departmentName = fullProfile ? fullProfile.unMappedUser.channel : ''
-        // if (this.activeRoute.snapshot.data.department.data) {
         if (fullProfile) {
           const leftData = this.activeRoute.snapshot.data.pageData.data.menus
           _.set(leftData, 'widgetData.logo', true)
