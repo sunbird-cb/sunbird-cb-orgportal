ca9ba7a36bbdd2a1741ed940376e33c1b338dbd6
NandiniAV <nandini.achutha@tarento.com>
2023-06-27 11:19:59 +0530

    Updated newrequests count in main BP listing page

--- project/ws/app/src/lib/routes/home/services/blended.service.ts
+++ project/ws/app/src/lib/routes/home/services/blended.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs'
 
 const API_END_POINTS = {
   GET_BLENDED_PROGRAMS: '/apis/proxies/v8/sunbirdigot/read',
+  GET_REQUESTS: '/apis/proxies/v8/workflow/blendedprogram/search',
 }
 
 @Injectable({
@@ -13,5 +14,9 @@ export class BlendedService {
   constructor(private http: HttpClient) { }
   getBlendedPrograms(request: any): Observable<any> {
     return this.http.post<any>(API_END_POINTS.GET_BLENDED_PROGRAMS, request)
+  }
+
+  getRequests(req: any) {
+    return this.http.post<any>(`${API_END_POINTS.GET_REQUESTS}`, req)
   }
 }
