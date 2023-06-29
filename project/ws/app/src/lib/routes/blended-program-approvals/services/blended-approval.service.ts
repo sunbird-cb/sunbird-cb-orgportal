cd777169db518fd1fcf65fc274b7f86162335f0c
Saurav Verma <saurav.verma@tarento.com>
2023-06-29 09:39:01 +0530

    MDO learner ui page

--- project/ws/app/src/lib/routes/blended-program-approvals/services/blended-approval.service.ts
+++ project/ws/app/src/lib/routes/blended-program-approvals/services/blended-approval.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
+import _ from 'lodash'
 
 const API_END_POINTS = {
   UPDATE_REQUEST: '/apis/proxies/v8/workflow/blendedprogram/update',
