7ca8ef7e8201ce5a33be41c40de1fbbe57133987
abhishek-K-P <abishek.koira@tarento.com>
2023-12-12 11:04:43 +0530

    still working on UI

--- src/app/component/root/root.component.ts
+++ src/app/component/root/root.component.ts
@@ -123,7 +123,7 @@ export class RootComponent implements OnInit, AfterViewInit {
         }
       }
       if (event instanceof NavigationStart) {
-        if (event.url.includes('preview') || event.url.includes('embed')) {
+        if (event.url.includes('embed')) {
           this.isNavBarRequired = false
         } else if (event.url.includes('author/') && this.isInIframe) {
           this.isNavBarRequired = false
