effae286094894b35358e8bc40e9d4f02ea6693f
vishnubansal <vishnu.bansal@tarento.com>
2023-12-13 15:23:20 +0530

    update for competency summary and popup yes action functionality

--- project/ws/app/src/lib/routes/training-plan/components/category-drop-down/category-drop-down.component.ts
+++ project/ws/app/src/lib/routes/training-plan/components/category-drop-down/category-drop-down.component.ts
@@ -22,6 +22,12 @@ export class CategoryDropDownComponent implements OnInit {
     // } else if(this.from === 'assignee') {
     //   this.handleCategorySelection.emit('Designation');
     // }
+    this.trainingPlanDataSharingService.trainingPlanCategoryChangeEvent.subscribe((data:any)=>{
+      if(data && data.event) {
+        console.log('data', data.event);
+        this.handleCategorySelection.emit(data.event);
+      }
+    })
   }
 
   ngOnChanges() {
@@ -45,9 +51,10 @@ export class CategoryDropDownComponent implements OnInit {
         dialogData['type'] = 'normal';
         dialogData['icon'] = 'radio_on';
         dialogData['title'] = 'You are attempting to change the selected content type?';
-        dialogData['subTitle'] = 'Test';
+        dialogData['subTitle'] = `Changing it now will result in the loss of your current selection. It's advisable to save the current one as a draft and create a new one instead.`;
         dialogData['primaryAction'] = 'I understand, change content type';
         dialogData['secondaryAction'] = 'Cancel';
+        dialogData['event'] = 'Course';
         this.trainingPlanDataSharingService.trainingPlanStepperData['contentType'] = event;
       break
       case 'Program':
@@ -57,6 +64,7 @@ export class CategoryDropDownComponent implements OnInit {
         dialogData['subTitle'] = `Changing it now will result in the loss of your current selection. It's advisable to save the current one as a draft and create a new one instead.`;
         dialogData['primaryAction'] = 'I understand, change content type';
         dialogData['secondaryAction'] = 'Cancel';
+        dialogData['event'] = 'Program';
         this.trainingPlanDataSharingService.trainingPlanStepperData['contentType'] = event;
       break
       case 'Blended program':
@@ -66,6 +74,7 @@ export class CategoryDropDownComponent implements OnInit {
         dialogData['subTitle'] = `Changing it now will result in the loss of your current selection. It's advisable to save the current one as a draft and create a new one instead.`;
         dialogData['primaryAction'] = 'I understand, change content type';
         dialogData['secondaryAction'] = 'Cancel';
+        dialogData['event'] = 'Blended program';
         this.trainingPlanDataSharingService.trainingPlanStepperData['contentType'] = event;
       break
       case 'Curated program':
@@ -75,6 +84,7 @@ export class CategoryDropDownComponent implements OnInit {
         dialogData['subTitle'] = `Changing it now will result in the loss of your current selection. It's advisable to save the current one as a draft and create a new one instead.`;
         dialogData['primaryAction'] = 'I understand, change content type';
         dialogData['secondaryAction'] = 'Cancel';
+        dialogData['event'] = 'Curated program';
         this.trainingPlanDataSharingService.trainingPlanStepperData['contentType'] = event;
       break
       case 'Moderated Course':
@@ -84,6 +94,7 @@ export class CategoryDropDownComponent implements OnInit {
         dialogData['subTitle'] = `Changing it now will result in the loss of your current selection. It's advisable to save the current one as a draft and create a new one instead.`;
         dialogData['primaryAction'] = 'I understand, change content type';
         dialogData['secondaryAction'] = 'Cancel';
+        dialogData['event'] = 'Moderated Course';
         this.trainingPlanDataSharingService.trainingPlanStepperData['contentType'] = event;
       break
       case 'Designation':
@@ -94,6 +105,7 @@ export class CategoryDropDownComponent implements OnInit {
         If you want to select custom users or by designation, use the above option`;
         dialogData['primaryAction'] = 'I understand, change user type';
         dialogData['secondaryAction'] = 'Cancel';
+        dialogData['event'] = 'Designation';
         this.trainingPlanDataSharingService.trainingPlanStepperData['assignmentType'] = event;
       break
       case 'All Users':
@@ -104,6 +116,7 @@ export class CategoryDropDownComponent implements OnInit {
         If you want to select custom users or by designation, use the above option`;
         dialogData['primaryAction'] = 'I understand, change user type';
         dialogData['secondaryAction'] = 'Cancel';
+        dialogData['event'] = 'All Users';
         this.trainingPlanDataSharingService.trainingPlanStepperData['assignmentType'] = event;
       break
       case 'Custom Users':
@@ -114,11 +127,12 @@ export class CategoryDropDownComponent implements OnInit {
         If you want to select custom users or by designation, use the above option`;
         dialogData['primaryAction'] = 'I understand, change user type';
         dialogData['secondaryAction'] = 'Cancel';
+        dialogData['event'] = 'Custom Users';
         this.trainingPlanDataSharingService.trainingPlanStepperData['assignmentType'] = event;
       break
     }
     
-    this.handleCategorySelection.emit(event);
+    // this.handleCategorySelection.emit(event);
     this.openDialoagBox(dialogData)
   }
 
@@ -131,6 +145,7 @@ export class CategoryDropDownComponent implements OnInit {
         subTitle: dialogData.subTitle,
         primaryAction: dialogData.primaryAction,
         secondaryAction: dialogData.secondaryAction,
+        event: dialogData.event
       },
     })
 
--- project/ws/app/src/lib/routes/training-plan/components/search/search.component.ts
+++ project/ws/app/src/lib/routes/training-plan/components/search/search.component.ts
@@ -69,14 +69,35 @@ export class SearchComponent implements OnInit {
       const filterObj = {
         "request": {
           "filters": {
-            "primaryCategory": ["Standalone Assessment"],
-            "contentType": [contentType]
+            "primaryCategory": ["Course"],
+            "contentType": [contentType],
+            "identifier": [
+              "do_1139104696922521601317",
+              "do_1135500940397936641158",
+              "do_1139026887251066881137",
+              "do_11374977269157888011298",
+              "do_113545216626319360174",
+              "do_1136643005068328961197",
+              "do_113694464635666432153",
+              "do_113882971081441280172",
+              "do_113882887752704000151",
+              "do_1138813223247462401116",
+              "do_113703698843983872134",
+              "do_1137468666262241281756",
+              "do_1139084314725498881268",
+              "do_113720205181059072151",
+              "do_113896199910367232144",
+              "do_11364656119311564815"
+            ]
           },
           "offset": 0,
           "limit": 20,
           "query": "",
           "sort_by": { "lastUpdatedOn": "desc" },
-          "fields": ["name", "appIcon", "instructions", "description", "purpose", "mimeType", "gradeLevel", "identifier", "medium", "pkgVersion", "board", "subject", "resourceType", "primaryCategory", "contentType", "channel", "organisation", "trackable", "license", "posterImage", "idealScreenSize", "learningMode", "creatorLogo", "duration", "version", "avgRating"]
+          "fields": ["name", "appIcon", "instructions", "description", "purpose", "mimeType", 
+          "gradeLevel", "identifier", "medium", "pkgVersion", "board", "subject", "resourceType", 
+          "primaryCategory", "contentType", "channel", "organisation", "trackable", "license", "posterImage", 
+          "idealScreenSize", "learningMode", "creatorLogo", "duration", "version", "avgRating", "competencies_v5"]
         }, "query": ""
       }
       this.trainingPlanService.getAllContent(filterObj).subscribe((res: any) => {
